bf85ac69cc0990114aa32045a8ad13eca50bd502
B7AST <85998893+B7AST@users.noreply.github.com>
2024-02-19 16:36:18 +0200

    Making Pages Selectable

--- pet_activity_log/src/App.js
+++ pet_activity_log/src/App.js
@@ -3,12 +3,22 @@ import { useState } from 'react';
 import NewPage from './components/NewPage.jsx';
 import NoPageSelected from './components/NoPageSelected.jsx';
 import ProjectSidebar from './components/ProjectsSidebar.jsx';
+import SelectedPage from './components/SelectedPage.jsx';
 
 function App() {
   const [pagesState, setPagesState] = useState({
     selectedPageId: undefined,
     pages: [],
   })
+
+  function selectPageHandler(id) {
+    setPagesState(prevState => {
+      return {
+        ...prevState,
+        selectedPageId: id,
+      };
+    })
+  }
 
   function addPageHandler() {
     setPagesState(prevState => {
@@ -43,7 +53,10 @@ function App() {
     })
   };
   
-  let content;
+  let selectedPage = pagesState.pages.find(page => page.dateId === pagesState.selectedPageId)
+  console.log(pagesState.pages)
+
+  let content = <SelectedPage page={selectedPage}/>;
 
   if (pagesState.selectedPageId === null) {
     content = <NewPage onAdd={addNewPageHandler} onCancel={addProjectCancelButtonHandler}/>
@@ -53,7 +66,7 @@ function App() {
 
   return (
     <main className="h-screen my-8 flex gap-8">
-      <ProjectSidebar onAddPageHandler={addPageHandler} pages={pagesState.pages}/>
+      <ProjectSidebar onAddPageHandler={addPageHandler} pages={pagesState.pages} onSelectPage={selectPageHandler}/>
       {content}
     </main>
   );
